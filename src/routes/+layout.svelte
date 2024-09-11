5ed5412b2f1fb6d9785447520707c54a8b6a84e2
Valyria <76444716+itsValyria@users.noreply.github.com>
2024-09-11 21:15:39 +0200

    first things fitrst

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+  import '../global.css';
+  // Hier komt gedeelde JavaScript logic voor elke pagina met deze lay-out
+</script>
+
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+</svelte:head>
+
+<header>
+  <!-- Hier komt de header / navigatie -->
+</header>
+
+<main>
+  <slot /> <!-- Hier wordt de pagina content geimplementeerd-->
+</main>
+
+<footer>
+  <!-- Hier komt de footer -->
+</footer>
+
+<style>
+  /* Hier komt gedeelde styles voor elke pagina met deze lay-out */
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,50 @@
 <script>
 export let data;
+
+function isValidImage(url) {
+    // Checking of de image url wel valid is
+    return /\.(jpg|jpeg|png|gif)$/i.test(url);
+  }
 </script>
 
-<ul>
-  {#each data.persons as person}
-  <li>
-    <img
-    class="main__card--background"
-    src={person.avatar && person.avatar !== '' ? person.avatar : '/placeholder.jpg'}
-    on:error="{() => this.src = '/placeholder.jpg'}"
-    alt="{person.name}"
-  />
-  </li>
-{/each}
-</ul>
+<main>
+  <ul>
+    {#each data.persons.filter(person => person.squad_id === 3) as person}
+    <li>
+      <a href="/{person.id}">
+          <img 
+          class="card-image" 
+          src={isValidImage(person.avatar) ? person.avatar : '/placeholder.jpg'} 
+          alt="{person.name}"
+          on:error="{(event) => event.target.src = '/placeholder.jpg'}"
+        />
+        <h2>{person.name}</h2>
+        <p>{person.prefix ? `${person.prefix} ${person.surname}` : person.surname}</p>
+      </a>
+    </li>
+    {/each}
+  </ul>
+</main>
 
 <style>
+  li {
+    background-color: var(--turquoise);
+    color: var(--blueberry);
 
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    box-sizing: border-box;
+
+    width: 10em;
+    height: 16em;
+  }
+
+  li .card-image {
+    height: 8rem;
+    width: 8rem;
+    object-fit: cover;
+    border-radius: 50%;
+  }
 </style>
