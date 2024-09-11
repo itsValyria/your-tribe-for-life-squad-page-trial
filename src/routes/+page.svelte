2190feb72f84a51b5440c4fa03cdd22f55dd1ed4
Valyria <76444716+itsValyria@users.noreply.github.com>
2024-09-11 20:45:08 +0200

    placeholder image works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,28 +7,12 @@ export let data;
   <li>
     <img
     class="main__card--background"
-    src={person.avatar && person.avatar !== '' ? person.avatar : 'assets/placeholder.jpg'}
-    on:error="{() => this.src = 'assets/placeholder.jpg'}"
+    src={person.avatar && person.avatar !== '' ? person.avatar : '/placeholder.jpg'}
+    on:error="{() => this.src = '/placeholder.jpg'}"
     alt="{person.name}"
   />
   </li>
 {/each}
-</ul>
-
-<ul>
-  {#each data.persons as person}
-    <li>
-      <strong>{person.name} {person.prefix} {person.surname}</strong>
-      
-      <!-- Check if avatar exists, else use a placeholder -->
-      <img
-        class="main__card--background"
-        src={person.avatar && person.avatar !== '' ? person.avatar : 'assets/placeholder.jpg'}
-        on:error="{() => this.src = 'assets/placeholder.jpg'}"
-        alt="{person.name}"
-      />
-    </li>
-  {/each}
 </ul>
 
 <style>
